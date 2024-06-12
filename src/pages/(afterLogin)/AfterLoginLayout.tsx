14230f4ce9b209bb38bc519aadbe356712948814
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-12 16:53:24 +0900

    [Fix]: redirect fix

--- src/pages/(afterLogin)/AfterLoginLayout.tsx
+++ src/pages/(afterLogin)/AfterLoginLayout.tsx
@@ -1,13 +1,14 @@
 import {Outlet, useNavigate} from "react-router-dom";
 import {useUserStore} from "@/store/userStore.ts";
 import {useEffect} from "react";
+import secureLocalStorage from "react-secure-storage";
 
 export default function AfterLoginLayout() {
   const { user } = useUserStore();
   const nav = useNavigate();
 
   useEffect(() => {
-    if(!user){
+    if(!user && secureLocalStorage.getItem('accessToken') === null){
       nav('/');
     }
   }, [user]);

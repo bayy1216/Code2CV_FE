ff78077aab0950dd44aa981aaf8c0d5911e88efa
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-12 13:19:35 +0900

    [Feat]: 분석 UI

--- src/pages/(afterLogin)/dashboard/ProjectPage.tsx
+++ src/pages/(afterLogin)/dashboard/ProjectPage.tsx
@@ -1,6 +1,7 @@
 import {useQuery} from "@tanstack/react-query";
 import {ProjectModel} from "@/api/project/project.response.ts";
 import {getProjects} from "@/api/project/project.api.ts";
+import {useState} from "react";
 
 export default function ProjectPage() {
   const {data} = useQuery<ProjectModel[]>({
@@ -21,6 +22,8 @@ export default function ProjectPage() {
 
 function ProjectList({projects}: {projects?: ProjectModel[]}) {
 
+  const [search, setSearch] = useState<string>("");
+
   if (!projects) {
     return (
       <div className="w-[640px] flex flex-col
@@ -32,26 +35,54 @@ function ProjectList({projects}: {projects?: ProjectModel[]}) {
 
   return (
     <div className="w-[640px] flex flex-col
-    items-center bg-white pt-12 rounded-[24px]">
-      {projects.map((project) => (
-        <ProjectItem key={project.id} project={project}/>
-      ))}
+    items-center bg-white pt-12 rounded-[24px] px-4">
+      <input
+        className="w-full h-[48px] bg-gray-100 rounded-[12px] text-[16px] p-4"
+        placeholder="분석할 레포지토리를 검색해보세요."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="h-[760px] overflow-y-auto w-full">
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project}/>
+        ))}
+      </div>
     </div>
   );
 }
 
 function ProjectItem({project}: {project: ProjectModel}) {
+  const updatedAt = new Date(project.updatedAt).toLocaleDateString();
+  const now = new Date().toLocaleDateString();
+  const daysDiff = Math.floor((new Date(now).getTime() - new Date(updatedAt).getTime()) / (1000 * 60 * 60 * 24));
+
+  const onButtonClick = () => {
+    console.log(project);
+  }
+
   return (
-    <div className="w-[640px] flex flex-row justify-between
-    items-center bg-white p-12 rounded-[24px]">
-      <h1 className="text-[20px] font-bold">{project.projectName}</h1>
-      {project.updatedAt}
+    <div className="w-full flex flex-row justify-between
+    items-center bg-white py-5 border-b px-4">
+      <h1 className="text-[16px] font-semibold flex-[3]">
+        {project.projectName}
+      </h1>
+      <div className="text-[14px] font-normal text-gray-1 flex-[4]">
+        Updated{daysDiff} days ago
+      </div>
+      <div className="">
+        <button
+          className="w-[72px] h-[33px] bg-bc text-white text-[14px] font-medium rounded-[8px]"
+          onClick={onButtonClick}
+        >
+          분석하기
+        </button>
+      </div>
     </div>
   );
 
 }
 
-function AfterAnalysisProjectList(){
+function AfterAnalysisProjectList() {
   return (
     <div className="w-[420px] flex flex-col p-[24px]
      bg-white rounded-[24px]">
